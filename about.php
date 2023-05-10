90d08be0d2a461c26a71bf2e60466c3ddf4be13f
InzamamIslamRaj\USER <inzamamislamraj@gmail.com>
2023-05-10 21:14:30 +0600

    first commit

---
+++ about.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>About Us | ABC University</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='statics/css/main.css'>
+</head>
+<body>
+    <header>
+        <h2><a href="home.php">ABC University</a></h2>
+        <ul class="nav">
+            <?php
+               include 'header.php';
+            ?>
+        </ul>
+    </header>
+    <div class="container base">
+        <div class="userinfo w550">
+            <h1>About Us</h1>
+            <p>ABC University (ABCU) established in 2001 is located in Dhaka Bangladesh. ABCU follows a liberal arts approach to education which nurtures fresh ideas and gives new impetus to the field of tertiary education. It ensures a high quality of education and aims to meet the demands of contemporary times. Building on ABCU's experience of seeking solutions to challenges posed by extreme poverty, ABCU hopes to instill in its students a commitment to working towards national development and progress. The medium of instruction and examination at ABC University is English. ABCU is accredited by the University Grants Commission (UGC) and approved by the Ministry of Education, Government of Bangladesh.</p>
+        </div>
+    </div>
+</body>
+</html>
